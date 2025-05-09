f640bee3173a0721221bdb8e6ae49274ee0105ec
nyujuzer <sallairobert94@gmail.com>
2025-05-09 11:47:58 +0200

    Add cookie-parser and resend dependencies; implement user registration. Issue: users cannot confirm email

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,11 +3,15 @@ import ViteExpress from "vite-express";
 import { createNewsArticle, upload } from "./news_genner";
 import dotenv from "dotenv";
 import { validate } from "uuid";
-import { check_API_key } from "./utils";
+import { check_API_key, check_user_exists, validate_email } from "./utils";
 import supabase from "./supabase";
+import cookieParser from "cookie-parser";
+import {Resend} from 'Resend'
 dotenv.config();
+const resend = new Resend(process.env.RESEND_KEY)
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.post("/create-articles", async (req, res) => {
   const key = req.body.api_key;
@@ -19,7 +23,7 @@ app.post("/create-articles", async (req, res) => {
     console.log("data", data);
     if (data && data.length == 0) {
       console.error("Error fetching API key:", error);
-      res.status(401).send({error:"Unauthorized"});
+      res.status(401).send({ error: "Unauthorized" });
     } else {
       const value = await createNewsArticle();
       console.log("value", value);
@@ -28,9 +32,9 @@ app.post("/create-articles", async (req, res) => {
   }
 });
 app.get("/articles", async (req, res) => {
-  if (req.query.tag){
-    res.redirect("/filter-articles?tag="+req.query.tag);
-    return
+  if (req.query.tag) {
+    res.redirect("/filter-articles?tag=" + req.query.tag);
+    return;
   }
   let { data: satirical_news_article, error } = await supabase
     .from("satirical_news_article")
@@ -42,7 +46,7 @@ app.get("/articles", async (req, res) => {
       "data:",
       satirical_news_article
     );
-    res.status(500).send({error:"Internal Server Error"});
+    res.status(500).send({ error: "Internal Server Error" });
   } else {
     res.send(satirical_news_article);
   }
@@ -50,19 +54,58 @@ app.get("/articles", async (req, res) => {
 app.get("/article/:id", async (req, res) => {
   const id = req.params.id;
   if (!validate(id)) {
-    res.status(400).send({error:"Invalid UUID"});
+    res.status(400).send({ error: "Invalid UUID" });
     return;
   }
   const { data, error } = await supabase
     .from("satirical_news_article")
     .select("*")
-    .eq("id", id);
+    .eq("id", id)
+    .single();
   if (error) {
     console.error("Error fetching articles:", error, "data:", data);
-    res.status(500).send({error:"Internal Server Error"});
+    res.status(500).send({ error: "Internal Server Error" });
   } else if (!data || data.length == 0) {
-    res.status(404).send({error:"Article not found"});
+    res.status(404).send({ error: "Article not found" });
   } else {
+    const userId = req.cookies?.id;
+    console.log(req.cookies);
+    if (userId) {
+      const { data: user_data, error } = await supabase
+        .from("users")
+        .select("tags")
+        .eq("user", userId)
+        .single();
+        console.log(user_data)
+      if (user_data && user_data.tags) {
+        const updatedTags = { ...user_data.tags };
+        data.tags.forEach((tag: string) => {
+          if (updatedTags[tag]) {
+            updatedTags[tag] += 1;
+          } else {
+            updatedTags[tag] = 1;
+          }
+        });
+        console.log(updatedTags, "________")
+        const { data: tag_success, error: updateError } = await supabase
+          .from("users")
+          .update({ tags: updatedTags })
+          .eq("user", userId)
+          .select();
+
+        console.log(tag_success);
+        if (updateError) {
+          console.log(
+            "Error updating user:",
+            updateError,
+            "data:",
+            tag_success
+          );
+        } else {
+          console.log("User updated successfully:", tag_success);
+        }
+      }
+    }
     res.send(data);
   }
 });
@@ -72,21 +115,29 @@ app.post("/create-article", async (req, res) => {
   if (key) {
     const key_is_valid = await check_API_key(key as string);
     if (key_is_valid.success) {
-      const upload_success = await upload({title:req.body.title,content:req.body.body, tags:req.body.tags as string[]});
-      res.send({success:true, message:"Article created successfully", upload_success});
-      return
+      const upload_success = await upload({
+        title: req.body.title,
+        content: req.body.body,
+        tags: req.body.tags as string[],
+      });
+      res.send({
+        success: true,
+        message: "Article created successfully",
+        upload_success,
+      });
+      return;
     } else {
       // const value = await createNewsArticle();
       // console.log("value", value);
       res.send(key_is_valid);
-      return
+      return;
     }
   }
   res.send(key);
 });
 
 app.get("/filter-articles", async (req, res) => {
-console.log("filter-articles")
+  console.log("filter-articles");
   const tag = req.query.tag?.toString().toUpperCase() as string;
   if (!tag) {
     res.redirect("/articles");
@@ -99,13 +150,47 @@ console.log("filter-articles")
     .contains("tags", [tags]);
   if (error) {
     console.error("Error fetching articles:", error, "data:", data);
-    res.status(500).send({error:"Internal Server Error"});
+    res.status(500).send({ error: "Internal Server Error" });
   } else if (!data || data.length == 0) {
-    res.status(404).send({error:"Article not found"});
+    res.status(404).send({ error: "Article not found" });
   } else {
     res.send(data);
   }
+});
 
+app.post("/register", async (req, res) => {
+  const {email, password, name} = req.body;
+  const {data: user_data, error: user_error} = await supabase.auth.signUp({email: email, password: password});
+  if (user_error){
+    console.error("Error signing up:", user_error);
+    res.status(400).send({error: "the email is already registered"});
+    return
+  }else{
+    const {data:user, error: insert_error,} = await supabase.from("users").insert({user:user_data.user?.id}).select().single()
+    if (insert_error) {
+      console.error("Error inserting user:", insert_error);
+      res.status(500).send({error: "Error creating new User"});
+      return
+    } else {
+      // const test = await resend.emails.send({
+      //   from:"test@test.com",
+      //   to:"sallairobert94@gmail.com",
+      //   subject:"hello world",
+      //   html:"Hello world"
+      // })
+      // console.log(test.data, test.error)
+      res.cookie("id",user?.id, {expires:new Date(Date.now()+86000000)}).send({success: true, message: "User created successfully", user});
+        return
+    }
+  }
+});
+app.post("/login", async (req, res)=>{
+  const {email, password} = req.body
+  if (email && password){
+    const {data, error} = await supabase.auth.signInWithPassword({email:email, password:password})
+    console.log(data, error)
+    res.send("hello")
+  }
 })
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
--- src/server/utils.ts
+++ src/server/utils.ts
@@ -17,5 +17,24 @@ const check_API_key = async (key: string) => {
     };
     return {success:true, message:"API key is valid", data}
 }
+const check_user_exists = async (email: string) => {
+  const { data, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("email", email);
+  if (error) {
+    console.error("Error fetching user:", error);
+    return {success:false, message:"Error fetching user", status:500}
+  } else if (!data || data.length == 0) {
+    return {success:false, message:"User not found", status:404}
+  } else {
+    return {success:true, message:"User exists", data}
+  }
+}
+const validate_email = (email: string) => {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  console.log("email", email, "re", re.test(email));
+  return re.test(email);
+}
 
-export {check_API_key}
+export {check_API_key, check_user_exists, validate_email}
